a9ab144ce3381ed8eba3464580464eba1c7c9933
Ben Zeitler <43448711+Ocarinauser@users.noreply.github.com>
2019-01-29 16:01:22 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+// Ben Zeitler 
+// Choose your own adventure 
+
+public class Main {
+	
+	public static void main (String[] args) {
+		
+		System.out.println("This is my integration project.");
+		System.out.println("It will be a decision making game type of project.");
+
+		
+		
+		
+	}
+}
+
+
+	  
+	  
+	  
+
+     
