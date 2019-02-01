90ad41f6b963413e3ddecf4615f4d06533bbc36d
Ben Zeitler <43448711+Ocarinauser@users.noreply.github.com>
2019-02-01 13:39:35 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,7 +12,7 @@ public class Main {
 	boolean whenAcurrate = true;	
         int howFast = 0;
 	double doubleDecimal = 2.0;
-        string firstName = "Ben";
+        String firstName = "Ben";
 	
 	
 		
