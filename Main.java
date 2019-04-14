f364496799b4484f7df5b98eb2bbc9c7defff84e
Ben Zeitler <43448711+Ocarinauser@users.noreply.github.com>
2019-04-14 14:36:34 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -356,26 +356,7 @@ public class Main {
 
   }
 
-  public class Person {
-
-    public Person(int a) {
-      System.out.println("In Person Constructor");
-      age = a;
-    }
-
-    protected int age;
-    // private int age;
-
-    public int getAge() {
-      return age;
-    }
-
-    public void setAge(int age) {
-      this.age = age;
-      System.out.println("I am in Person");
-    }
-
-  }
+ 
 
 }
 
