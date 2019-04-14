77ea28ae945063087b2297b1cfd3bf2acbe0cc9e
Ben Zeitler <43448711+Ocarinauser@users.noreply.github.com>
2019-04-14 14:34:42 -0400

    Create person.java

---
+++ person.java
@@ -0,0 +1,20 @@
+public class Person {
+
+    public Person(int a) {
+      System.out.println("In Person Constructor");
+      age = a;
+    }
+
+    protected int age;
+    // private int age;
+
+    public int getAge() {
+      return age;
+    }
+
+    public void setAge(int age) {
+      this.age = age;
+      System.out.println("I am in Person");
+    }
+
+  }
