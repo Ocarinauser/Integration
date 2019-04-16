48b19ccd04492d543e6bd423386cd08fb2266bb9
Ben Zeitler <43448711+Ocarinauser@users.noreply.github.com>
2019-04-16 15:09:33 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 // Ben Zeitler
 // Theme is Choose your own adventure
@@ -13,20 +15,20 @@ import java.util.ArrayList;
 // int: is a 32 bit signed two's complement integer. Minimum of -2^31 and Maximum of 2^31-1. Use
 // integer class to use int data type as an unsigned integer.
 // Long: 64 bit two's complement integers. The signed long has a minimum of -2^63 and a maximum
-// value of 263-1. In Java SE 8 and later, you can use the longdata type to represent an unsigned
+// value of 263-1. In Java SE 8 and later, you can use the longdata type to represent an unsigned
 // 64-bit long, which has a minimum value of 0 and a maximum value of 264-1. Use this data type when
-// you need a range of values wider than those provided by int. this class also contains methods.
+// you need a range of values wider than those provided by int. this class also contains methods.
 // Float: is a single-precision 32 nit IEEE 754 floating point. Specified in the Floated-Point
 // Types, Formats, and Values section of the Java Language Specification. Like byte and short, use a
 // float if you need to save memory in large arrays of floating point numbers. Should never be used
 // for precise values, like currency.
 // Double: double precision 64 hit IEEE 754 floating point. For decimal values, this data type is
 // generally the default choice. Never be used for precise values.
-// Boolean: has only two possible values: true and false. Use this data type for simple true/false
+// Boolean: has only two possible values: true and false. Use this data type for simple true/false
 // conditions. This data type represents one bit of information, but its "size" isn't something
 // that's precisely defined.
-// Char: single 16-bit Unicode character. It has a minimum value of '\u0000' (or 0) and a maximum
-// value of '\uffff'
+// Char: single 16-bit Unicode character. It has a minimum value of '\u0000' (or 0) and a maximum
+// value of '\uffff'
 
 // Variable is a location in memory
 // Scope means a variable is only accessible in the method it was declared.
@@ -247,7 +249,7 @@ public class Main {
     System.out.println("My roommate said  \"Get more papertowels!\" " + "to " + firstName);
     // Static means belonging to a class not an object
 
-   // PSI 3 starts here
+    // PSI 3 starts here
 
     // Inheritance is the process where a class acquires properties (methods and fields) of another.
     // You can reuse the fields and methods of the existing class without having to write and debug
@@ -258,57 +260,54 @@ public class Main {
     int[] anArray = {1, 2, 3, 4, 5};
     for (int i = 0; i < anArray.length; i++) {
       System.out.println(anArray[i]);
-      //manually find the smallest value in an array
+      // manually find the smallest value in an array
       System.out.println(anArray[0]);
     }
-    
+
     // Get a sum of values in an array using accumulator
     int someArray[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int sum = 0;
 
     int[] ta = new int[101];
     for (int t : someArray)
-        sum += t;
-    for (int t = 0; t < ta.length; t++) ta[t] = t;
-       for (int e : ta) sum += e;
+      sum += t;
+    for (int t = 0; t < ta.length; t++)
+      ta[t] = t;
+    for (int e : ta)
+      sum += e;
     System.out.println(sum);
-    
+
     // Declare and use an ArrayList
-    ArrayList<Integer>theNumbers= new ArrayList<Integer>();
-    
+    ArrayList<Integer> theNumbers = new ArrayList<Integer>();
+
     theNumbers.add(10);
     theNumbers.add(20);
     theNumbers.add(30);
-    
-    System.out.println(theNumbers.get(0));
-    
-    //Search an array
-    
-    List<Integer> array = Arrays.asList(1, 3, 5, 2, 4);
-        if (array.contains(4)) {
-            System.out.println("We found 4");
-        };
 
-        
-    
+    System.out.println(theNumbers.get(0));
+
+    // Search an array
+    List<Integer> array = Arrays.asList(1, 3, 5, 2, 4);
+    if (array.contains(4)) {
+      System.out.println("We found 4");
+    } ;
+
+
+
     // use multi-dimensional arrays
-    
+
     int[] values = {4, 7, 17};
-    
+
     System.out.println(values[2]);
-    
-    int[][] grid = {
-    		{4, 7, 17},
-    		{3, 6},
-    		{1, 2, 3, 4}
-    };
+
+    int[][] grid = {{4, 7, 17}, {3, 6}, {1, 2, 3, 4}};
     System.out.println(grid[1][1]);
     System.out.println(grid[0][1]);
     // Search an array and identify index
     // It will print out 17 at number 7
     // prints out 7 in grid 0
-    
-    
+
+
     // Search a two-dim array
     // create and use the enhanced for loop
 
@@ -341,7 +340,4 @@ public class Main {
 
   }
 
-  }
-
-
-
+}
