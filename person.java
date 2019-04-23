79d771d4eb699cc343790cd3b1e69ea9b6b9ec0a
Ben Zeitler <43448711+Ocarinauser@users.noreply.github.com>
2019-04-23 15:21:59 -0400

    Update person.java

--- person.java
+++ person.java
@@ -1,20 +1,23 @@
 public class Person {
 
-    public Person(int a) {
-      System.out.println("In Person Constructor");
-      age = a;
-    }
-
-    protected int age;
-    // private int age;
-
-    public int getAge() {
-      return age;
-    }
-
-    public void setAge(int age) {
-      this.age = age;
-      System.out.println("I am in Person");
-    }
-
+  public Person(int a) {
+    System.out.println("In Person Constructor");
+    age = a;
   }
+
+  protected int age;
+  // private int age;
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+    System.out.println("I am in Person");
+  }
+
+  public void nogender() {
+    System.out.println("I am a person!");
+  }
+}
